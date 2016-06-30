80dedf0179b08fcf709f9fbdcf5d1b25646b2209
Jens Helderweirdt <jens.helderweirdt@gmail.com>
2016-06-30 14:56:57 +0200

    Upgrade to .NET Core 1.0

---
+++ src/Digipolis.Toolbox.Errors/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Digipolis.Toolbox.Errors.Internal;
+
+namespace Digipolis.Toolbox.Errors.Exceptions
+{
+    public class NotFoundException : BaseException
+    {
+        public NotFoundException() : base(Defaults.NotFoundException.Message)
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(Error error) : base(error, Defaults.NotFoundException.Message)
+        {
+        }
+    }
+}
---
+++ src/Digipolis.Toolbox.Errors/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Digipolis.Toolbox.Errors.Internal;
+
+namespace Digipolis.Toolbox.Errors.Exceptions
+{
+    public class ValidationException : BaseException
+    {
+        public ValidationException() : base(Defaults.ValidationException.Message)
+        {
+        }
+
+        public ValidationException(string message) : base(message)
+        {
+        }
+
+        public ValidationException(Error error) : base(error, Defaults.ValidationException.Message)
+        {
+        }
+    }
+}
